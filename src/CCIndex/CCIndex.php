40d41d02dabdc877c9345958034464f416f157a2
Henrik Roden <karlhenrikroden@gmail.com>
2013-04-26 21:09:06 +0200

    kmom5

--- src/CCIndex/CCIndex.php
+++ src/CCIndex/CCIndex.php
@@ -1,30 +1,46 @@
 <?php
 /**
- * Standard controller layout.
- * 
- * @package LydiaCore
- */
-class CCIndex implements IController {
+* Standard controller layout.
+*
+* @package LydiaCore
+*/
+class CCIndex extends CObject implements IController {
 
   /**
-    * Implementing interface IController. All controllers must have an index action.
-   */
-  public function Index() {  
-    global $ly;
-    $ly->data['title'] = "The Index Controller";
-    $ly->data['main'] = "<h1>The Index Controller</h1>";
+* Constructor
+*/
+  public function __construct() {
+    parent::__construct();
   }
   
+
+  /**
+* Implementing interface IController. All controllers must have an index action.
+*/
+  public function Index() {	
+    $this->views->SetTitle('Index Controller');
+    $this->views->AddInclude(__DIR__ . '/index.tpl.php', array('menu'=>$this->Menu()));
+  }
+
+
+  /**
+* A menu that shows all available controllers/methods
+*/
+  private function Menu() {	
+    $items = array();
+    foreach($this->config['controllers'] as $key => $val) {
+      if($val['enabled']) {
+        $rc = new ReflectionClass($val['class']);
+        $items[] = $key;
+        $methods = $rc->getMethods(ReflectionMethod::IS_PUBLIC);
+        foreach($methods as $method) {
+          if($method->name != '__construct' && $method->name != '__destruct' && $method->name != 'Index') {
+            $items[] = "$key/" . mb_strtolower($method->name);
+          }
+        }
+      }
+    }
+    return $items;
+  }
   
-         /*private function Menu() {   
-          $ly = CLydia::Instance();
-
-          // ... some code removed
-            $html .= "<li><a href='" . $ly->request->CreateUrl($val) . "'>$val</a>"; 
-
-          // ... some code removed
-          $ly->data['title'] = "The Index Controller";
-
-  */
-}
-?>
+} 
